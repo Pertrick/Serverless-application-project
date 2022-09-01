24f4c2b0b6d11b4650db41bdbe928b68f61d33f4
Patrick Udoh <udohpertrick@gmail.com>
2022-09-01 11:50:57 -0400

    serverless application

---
+++ README.md
@@ -0,0 +1,2 @@
+# Getting Started
+"# Serverless-application-project" 
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'l6sq8byvb2';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-e-1si858.us.auth0.com',
+  clientId: '9bqd0Ui5gSsv4hvvCQVN1z7d7SZIk5H4',
+  callbackUrl: 'http://localhost:3000/callback'
+};
